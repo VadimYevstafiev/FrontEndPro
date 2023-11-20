a01d8cb8be3b113d008596a1ddbc91f32eadd3ba
Vadim Yevstafiev <eustas1976@gmail.com>
2023-11-20 17:34:44 +0200

    Homework_3

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const x = prompt('What is your name?');
-alert(`Hello, ${x} !How are you?`);
+const x1 = prompt('Write any number');
+const x2 = prompt('Write any other number');
+const x3 = prompt('Write any any other number');
+alert(`${x1} ${x2} ${x3}`);
+
+let x = 54678;
+let str = '';
+let rest;
+
+
+
+while (x > 0) {
+    rest = x % 10;
+    str = rest + ' ' + str;
+    x = (x - rest) / 10;
+}
+
+alert(str);
