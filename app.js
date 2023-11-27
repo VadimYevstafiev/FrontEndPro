f5646021f430571007d87b4ba52d602852b185ba
Vadim Yevstafiev <eustas1976@gmail.com>
2023-11-27 19:27:29 +0200

    Homework_05

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const x1 = Number(prompt('Write any number'));
-const x2 = Number(prompt('Write any other number'));
+const operand = String(prompt('What do you want?'));
+const x1 = Number(prompt('Enter first number'));
+const x2 = Number(prompt('Enter second number'));
 
+let y;
 
-const y1 = x1 + x2;
-const y2 = x1 - x2;
-const y3 = x1 * x2;
-const y4 = x1 / x2;
-
-alert(`${x1} + ${x2} = ${y1}\n\n
-    ${x1} - ${x2} = ${y2}\n\n
-    ${x1} * ${x2} = ${y3}\n\n
-    ${x1} / ${x2} = ${y4}\n\n`);
-
+switch (operand) {
+    case 'add':
+        y = x1 + x2;
+        alert(`${x1} + ${x2} = ${y}\n\n`);
+        break;
+    case 'sub':
+        y = x1 - x2;
+        alert(`${x1} - ${x2} = ${y}\n\n`);
+        break;
+    case 'mult':
+        y = x1 * x2;
+        alert(`${x1} * ${x2} = ${y}\n\n`);
+        break;
+    case 'div':
+        y = x1 / x2;
+        alert(`${x1} / ${x2} = ${y}\n\n`);
+        break;
+}
