137a40f7e151f911df09d62efd9229991273049c
Vadim Yevstafiev <eustas1976@gmail.com>
2023-11-30 10:39:43 +0200

    Homework_08

--- app.js
+++ app.js
@@ -1,45 +1,9 @@
-const year = prompt('Enter year of your birth');
-const city = prompt('What city do you live in?');
-const sport = prompt('What is your favorite sport?');
+const x1 = Number(prompt('Enter first number'));
+const x2 = Number(prompt('Enter second number'));
+const x3 = Number(prompt('Enter third number'));
 
-let ageMessage = year ? `Your age is ${2023 - year}` : 'It is a pity that you did not want to enter year of your birth';
-let cityMessage;
-let sportMessage;
+const count = 3;
 
-switch (city) {
-    case 'Kiev':
-        cityMessage = 'You live in capital of Ukraine';
-        break;
-    case 'London':
-        cityMessage = 'You live in capital of Grait Britain';
-        break;
-    case 'Washington':
-        cityMessage = 'You live in capital of USA';
-        break;
-    case null:
-        cityMessage = 'It is a pity that you did not want to enter your city';
-        break;
-    default:
-        cityMessage = `You live in ${city}`;
-        break;
-}
+const result = (x1 + x2 + x3) /count;
 
-switch (sport) {
-    case 'box':
-        sportMessage = 'Cool! Do you want to become like Mohammad Ali?';       
-        break;
-    case 'football':
-        sportMessage = 'Cool! Do you want to become like Pele?';       
-        break;
-    case 'Formula 1':
-        sportMessage = 'Cool! Do you want to become Schumacher?';       
-        break;
-    case null:
-        sportMessage = 'It is a pity that you did not want to enter your sport';
-        break;
-    default:
-        sportMessage = ``;
-        break;
-}
-
-alert(`* ${ageMessage} \n\n * ${cityMessage} \n\n * ${sportMessage} \n\n`);
+alert(`* The arithmetic mean is equal to ${result} \n\n `);
