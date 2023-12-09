a9fb2ae79877d27b06ed3c5f333e9d5152ad53a4
Vadim Yevstafiev <eustas1976@gmail.com>
2023-12-09 21:56:53 +0200

    Homework_15

--- app.js
+++ app.js
@@ -1,76 +1,11 @@
-let users = [
-    {
-        "index": 0,
-        "isActive": true,
-        "balance": "$2,226.60",
-        "name": "Eugenia Sawyer",
-        "gender": "female",
-        "phone": "+1 (840) 583-3207",
-        "address": "949 John Street, Rose, Puerto Rico, 1857"
-    },
-    {
-        "index": 1,
-        "isActive": true,
-        "balance": "$2,613.77",
-        "name": "Pauline Gallegos",
-        "gender": "female",
-        "phone": "+1 (985) 593-3328",
-        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-    },
-    {
-        "index": 2,
-        "isActive": false,
-        "balance": "$3,976.41",
-        "name": "Middleton Chaney",
-        "gender": "male",
-        "phone": "+1 (995) 591-2478",
-        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-        "index": 3,
-        "isActive": true,
-        "balance": "$1,934.58",
-        "name": "Burns Poole",
-        "gender": "male",
-        "phone": "+1 (885) 559-3422",
-        "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-        "index": 4,
-        "isActive": true,
-        "balance": "$3,261.65",
-        "name": "Mcfadden Horne",
-        "gender": "male",
-        "phone": "+1 (942) 565-3988",
-        "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-        "index": 5,
-        "isActive": false,
-        "balance": "$1,790.56",
-        "name": "Suzette Lewis",
-        "gender": "female",
-        "phone": "+1 (837) 586-3283",
-        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
-    }
-]
+function removeElement(array, item) {
+    return array.filter(function (value) {
+        return value !== item;
+    });
+}
 
-const updatedUsers = users.map(function (user) {
-    balance = user.balance;
-    return {...user, numBalance:Number(balance.slice(1).replaceAll(',', ''))};
-    
-});
-
-const richUsers = updatedUsers.filter(function (user) {
-    return user.numBalance > 2000
-});
-
-const richUserPhones = richUsers.map(function (user) {
-        return user.phone;
-});
-console.log(richUserPhones);
-
-const sum = richUsers.reduce(function (sum, user) {
-    return Math.round((sum + user.numBalance) * 100) / 100;
-}, 0);
-console.log(sum);
+const array = [1, 2, 3, 4, 5, 6, 7];
+const newArray = removeElement(array, 5);
+console.log(array);
+console.log(newArray);
+// Результат: [1, 2, 3, 4, 6, 7]
