c83de4b4834c394708c329e2cca3f515c8e8e7be
Vadim Yevstafiev <eustas1976@gmail.com>
2023-12-14 22:14:55 +0200

    Homework_19

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const num = Number(prompt('Enter number'));
-const degree = Number(prompt('Enter degree'));
+function getSum(term) {
+    let sum = 0;
 
-console.log(power(num, degree));
-
-function power(num, degree) {
-    if(degree == 0) return 1;
-    if (degree === 1) return num;
-    if (degree === -1) return 1 / num;
-
-    if (degree > 0) {
-        return num * power(num, --degree);
-    } else {
-        return power(num, ++degree) / num;
+    return function (term) {
+        sum += term;
+        return sum;
     }
 }
+
+const sum = getSum();
+
