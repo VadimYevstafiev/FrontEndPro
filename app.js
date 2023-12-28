94fbb51907fc348c3dcd5921af3a0338266e1f58
Vadim Yevstafiev <eustas1976@gmail.com>
2023-12-28 22:19:41 +0200

    Homework_22

--- app.js
+++ app.js
@@ -1,60 +1,90 @@
-class Human {
-    constructor(name, gender) {
-        this.name = name;
-        this.gender = gender;
-    }
-}
+class Hamburger {
+    static SIZE_BIG = Math.random();
+    static SIZE_SMALL = Math.random();
 
-class Appartment {
-    people = [];
+    static STUFFING_CHEESE = Math.random();
+    static STUFFING_SALAD = Math.random();
+    static STUFFING_POTATO = Math.random();
 
-    addHuman(human) {
-        this.people.push(human);
-    }
-}
+    static TOPPING_SAUCE = Math.random();
+    static TOPPING_MAYO = Math.random();
 
-class Building {
-    appartments = [];
+    constructor(size, stuffing) {
+        this.price = [];
+        this.calories = [];       
 
-    constructor(maxAppart) {
-        this.maxAppart = maxAppart;
-    }
-
-    addAppartment(appartment) {
-        if (this.appartments.length >= this.maxAppart) {
-            console.log('Building overflow!');
-            return;
+        switch (size) {
+            case Hamburger.SIZE_BIG:
+                this.price.push(100);
+                this.calories.push(40);
+                break;
+            case Hamburger.SIZE_SMALL:
+                this.price.push(50);
+                this.calories.push(20);
+                break;        
+            default:
+                console.log('Uncorrect size');
+                return;
         }
-        this.appartments.push(appartment);
+
+        switch (stuffing) {
+            case Hamburger.STUFFING_CHEESE:
+                this.price.push(10);
+                this.calories.push(20);
+                break;
+            case Hamburger.STUFFING_SALAD:
+                this.price.push(20);
+                this.calories.push(5);
+                break;
+            case Hamburger.STUFFING_POTATO:
+                this.price.push(15);
+                this.calories.push(10);
+                break;        
+            default:
+                console.log('Uncorrect stuffing');
+                return;
+        }
+    }
+
+    addTopping($topping) {
+        switch ($topping) {
+            case Hamburger.TOPPING_SAUCE:
+                this.price.push(15);
+                this.calories.push(0);
+                break;
+            case Hamburger.TOPPING_MAYO:
+                this.price.push(20);
+                this.calories.push(5);
+                break;        
+            default:
+                console.log('Uncorrect topping');
+                return;
+        }
+    }
+
+    calculate() {
+        return this.calories.reduce((total, item) => total + item);
+    }
+
+    calculatePrice() {
+        return this.price.reduce((total, item) => total + item);
     }
 }
 
-let Masha = new Human('Masha', 'female');
-let Dasha = new Human('Dasha', 'female');
-let Petya = new Human('Petya', 'male');
-let Kolya = new Human('Kolya', 'male');
-let Archibald = new Human ('Archibald', undefined);
+// маленький гамбургер з начинкою з сиру
+var hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
 
-let numberOne = new Appartment();
-numberOne.addHuman(Masha);
-numberOne.addHuman(Petya);
+// добавка з майонезу
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
 
-let numberTwo = new Appartment();
-numberTwo.addHuman(Dasha);
+// запитаємо скільки там калорій
+console.log("Calories: " + hamburger.calculate ());
 
-let numberThree = new Appartment();
-numberThree.addHuman(Kolya);
+// скільки коштує
+console.log("Price: " + hamburger.calculatePrice());
 
-let numberFour = new Appartment();
+// я тут передумав і вирішив додати ще приправу
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
 
-let numberFive = new Appartment();
-numberFive.addHuman(Archibald);
-
-let house = new Building(4);
-house.addAppartment(numberOne);
-house.addAppartment(numberTwo);
-house.addAppartment(numberThree);
-house.addAppartment(numberFour);
-house.addAppartment(numberFive);
-
-console.log(house);
+// А скільки тепер коштує?
+console.log("Price with sauce: " + hamburger.calculatePrice());
