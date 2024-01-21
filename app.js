a65708b2bdb14bea595bbd07b6764f03d8953ac9
Vadim Yevstafiev <eustas1976@gmail.com>
2024-01-21 20:16:57 +0200

    Homework_31

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-var codes = {
-    "+6": "Польща",
-    "+38": "Україна",
-    "+1": "США"
-   };
-for (let key in codes) {
-    let value = codes[key];
-    key = +key;
-    console.log(`${key}: ${value}`);
-}
+const reviews = [
+    new Map([
+        ['name', 'Катя'],
+        ['text', 'Катя не катит']
+    ]),
+    new Map([
+        ['name', 'Валя'],
+        ['text', 'Валя не валит']
+    ]),
+    new Map([
+        ['name', 'Света'],
+        ['text', 'Света не светит']
+    ]),
+];
+
+const parent = document.querySelector('#testimonials');
+
+const insert = new DocumentFragment();
+
+reviews.map(function (item) {
+    const child = document.createElement('div');
+    
+    const head = document.createElement('h1');
+    head.innerHTML = item.get('name');
+    child.append(head);
+
+    const content = document.createElement('p');
+    content.innerHTML = item.get('text');
+    child.append(content);
+
+    insert.append(child);
+})
+
+parent.append(insert);
