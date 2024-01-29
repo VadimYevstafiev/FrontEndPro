b9ab9980c9c9f3c5a6c990554e88ef46283e96ab
Vadim Yevstafiev <eustas1976@gmail.com>
2024-01-29 20:49:53 +0200

    Homework_34

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+ function createSmileCell(number) {
+    const smile = document.createElement('IMG');
+    smile.src = `./images/smile_${number}.png`;
+    smile.classList.add('smile');
+
+    const cell = document.createElement('TH');
+    cell.append(smile);
+    cell.setAttribute(`data-id`, number);
+    return cell;
+ }
+
+ function createRateCell(number) {
+    const rate = document.createElement('DIV');
+    rate.innerText = rating[number];
+    rate.classList.add('rate');
+    rate.setAttribute(`id`, number);
+
+    const cell = document.createElement('TD');
+    cell.append(rate);
+    return cell;
+ }
+
+ function createRow(filler) {
+    const row = document.createElement('TR');
+    for (let i = 0; i < 5; i++) {
+        row.append(filler(i));
+    }
+    return row;
 }
 
+const rating = [0, 0, 0, 0, 0];
+const table = document.createElement('TABLE');
+table.append(createRow(createSmileCell));
+table.append(createRow(createRateCell));
 
-const btn = document.querySelector("#btn");
-btn.addEventListener('click', function () {
-    const box = document.querySelector('#image');
-    box.innerHTML = '';
-    const rndInt = randomIntFromInterval(1, 9);
-    const image = document.createElement('img');
-    image.src = `./images/${rndInt}.jpg`;
-    box.append(image);
+const parent = document.querySelector('#table');
+parent.append(table);
 
-})
+table.addEventListener('click', function (event) {
+    const th = event.target.closest('th');
 
+    if (!th) return;
+
+    if (!table.contains(th)) return;
+
+    const id = th.dataset.id;
+
+    const result = document.getElementById(`${id}`);
+    result.innerText = ++rating[id];
+});
