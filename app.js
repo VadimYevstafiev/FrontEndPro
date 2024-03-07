1a3a32da1e7da0c0f0ee91af6241c073c8f9b0ad
Vadim Yevstafiev <eustas1976@gmail.com>
2024-03-07 20:44:35 +0200

    Homework_42

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+const min = 1;
+const max = 9;
+
+function renderImage(box, number) {
+	box.innerHTML = '';
+	box.setAttribute(`data-number`, number);
+	const image = document.createElement('img');
+	image.src = `./images/${number}.jpg`;
+	box.append(image);
 }
 
+const container = document.querySelector("#container");
+container.addEventListener('click', function (event) {
+	const box = this.querySelector('#image');
+	let number = box.dataset.number;
 
-const btn = document.querySelector("#btn");
-btn.addEventListener('click', function () {
-    const box = document.querySelector('#image');
-    box.innerHTML = '';
-    const rndInt = randomIntFromInterval(1, 9);
-    const image = document.createElement('img');
-    image.src = `./images/${rndInt}.jpg`;
-    box.append(image);
+	const button = event.target.closest('button');
 
-})
+	if (!button) return;
+
+    if (!container.contains(button)) return;
+
+	switch (button.id) {
+		case 'prev':
+			number--;
+			break;	
+		case 'next':
+			number++;
+			break;
+	}
+
+	renderImage(box, number);
+
+	switch (number) {
+		case min:
+			this.querySelector("#prev").style.display = "none";
+			break;
+		case max:
+			this.querySelector("#next").style.display = "none";
+			break;
+		default:
+			this.querySelector("#prev").style.display = "block";
+			this.querySelector("#next").style.display = "block";
+			break;
+	}
+});
+
+
+document.querySelector("#prev").style.display = "none";
+renderImage(document.querySelector('#image'), 1);
 
